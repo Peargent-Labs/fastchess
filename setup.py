8bd90a52f962a165580b40445a43f670e09a7b6f
Quanta-Naut <123290216+Quanta-Naut@users.noreply.github.com>
2026-05-29 09:11:43 +0000

    Fix Windows build: use MSVC-compatible compiler flags

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, Extension
 import numpy as np
+import sys
+
+if sys.platform == "win32":
+    compile_args = ["/O2", "/W3"]
+else:
+    compile_args = ["-O3", "-Wall", "-Wextra", "-Wno-unused-parameter"]
 
 setup(
     ext_modules=[
@@ -7,7 +13,7 @@ setup(
             "fastchess",
             sources=["fastchess.c"],
             include_dirs=[np.get_include()],
-            extra_compile_args=["-O3", "-Wall", "-Wextra", "-Wno-unused-parameter"],
+            extra_compile_args=compile_args,
         ),
     ],
 )
